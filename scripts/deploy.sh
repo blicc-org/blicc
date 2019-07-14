cc7d1de30cdfe98829ae2a944c2ad2f34058dcb7
Thilo Ilg <contact@thiloilg.com>
2019-07-14 14:45:22 +0200

    feat(all): use own docker registry

--- scripts/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# deploy a docker image
-# $1: image name, $2: path to Dockerfile
-docker build -t $1 $2
-docker save -o ../docker/$1.tar $1
-scp -r $PWD/../docker/$1.tar root@ip:/root/
-ssh -tt root@ip "docker load --input /root/$1.tar"
-ssh -tt root@ip "docker-compose -f /root/docker-compose.yml up -d --force-recreate"
--- scripts/replace.sh
+++
@@ -1,6 +0,0 @@
-
-while read LINE; do
-    key="$(cut -d'=' -f1 <<<"$LINE")"
-    value="$(cut -d'=' -f2 <<<"$LINE")"
-    sed -i '' -e "s/$key/$value/g" traefik.toml
-done < ../.env.local
