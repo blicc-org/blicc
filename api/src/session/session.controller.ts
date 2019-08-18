068d677f5662c7d04e047b93fdf836fac435aa2c
Thilo Ilg <contact@thiloilg.com>
2019-08-18 18:53:07 +0200

    test(api): cookies domain

--- api/src/session/session.controller.ts
+++ api/src/session/session.controller.ts
@@ -39,9 +39,9 @@ export class SessionController {
     ctx.cookies.set('access_token', token, {
       maxAge,
       domain: 'api.blicc.org',
-      secure: false, // set true on prod
+      secure: true, // set true on prod
       overwrite: true,
-      httpOnly: false,
+      httpOnly: true,
     })
 
     ctx.status = status.ACCEPTED
