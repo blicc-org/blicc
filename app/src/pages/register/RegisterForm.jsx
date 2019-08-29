2607d74cca06b977120dcb0f7f1a7361f6462954
Thilo Ilg <contact@thiloilg.com>
2019-08-29 22:23:37 +0200

    feat(app): register page design update

--- app/src/pages/register/RegisterForm.jsx
+++ app/src/pages/register/RegisterForm.jsx
@@ -2,127 +2,85 @@ import React from 'react'
 
 export default function RegisterForm() {
   return (
-    <div className="container">
-      <div className="py-5 text-center">
-        <div className="col-md-8 order-md-1">
-          <h4 className="mb-3">Billing address</h4>
-          <form className="needs-validation" novalidate>
-            <div className="row">
-              <div className="col-md-6 mb-3">
-                <label for="firstName">First name</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="firstName"
-                  placeholder=""
-                  value=""
-                  required
-                />
-                <div className="invalid-feedback">
-                  Valid first name is required.
-                </div>
-              </div>
-              <div className="col-md-6 mb-3">
-                <label for="lastName">Last name</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="lastName"
-                  placeholder=""
-                  value=""
-                  required
-                />
-                <div className="invalid-feedback">
-                  Valid last name is required.
-                </div>
-              </div>
+    <div
+      className="col-md-4 mx-auto py-5 my-5 text-center"
+      style={{ maxWidth: '500px' }}
+    >
+      <h1 className="h3 mb-3 font-weight-normal">Register</h1>
+      <form className="needs-validation" novalidate>
+        <div className="row">
+          <div className="col-md-6 mb-3">
+            <label htmlFor="firstName">First name</label>
+            <input
+              type="text"
+              className="form-control"
+              id="firstName"
+              placeholder="John"
+              value=""
+              required
+            />
+            <div className="invalid-feedback">
+              Valid first name is required.
             </div>
-            <div className="mb-3">
-              <label for="email">
-                Email <span className="text-muted">(Optional)</span>
-              </label>
-              <input
-                type="email"
-                className="form-control"
-                id="email"
-                placeholder="you@example.com"
-              />
-              <div className="invalid-feedback">
-                Please enter a valid email address for shipping updates.
-              </div>
-            </div>
-            <div className="mb-3">
-              <label for="address">Address</label>
-              <input
-                type="text"
-                className="form-control"
-                id="address"
-                placeholder="1234 Main St"
-                required
-              />
-              <div className="invalid-feedback">
-                Please enter your shipping address.
-              </div>
-            </div>
-            <div className="mb-3">
-              <label for="address2">
-                Address 2 <span className="text-muted">(Optional)</span>
-              </label>
-              <input
-                type="text"
-                className="form-control"
-                id="address2"
-                placeholder="Apartment or suite"
-              />
-            </div>
-            <div className="row">
-              <div className="col-md-5 mb-3">
-                <label for="country">Country</label>
-                <select
-                  className="custom-select d-block w-100"
-                  id="country"
-                  required
-                >
-                  <option value="">Choose...</option>
-                  <option>United States</option>
-                </select>
-                <div className="invalid-feedback">
-                  Please select a valid country.
-                </div>
-              </div>
-              <div className="col-md-4 mb-3">
-                <label for="state">State</label>
-                <select
-                  className="custom-select d-block w-100"
-                  id="state"
-                  required
-                >
-                  <option value="">Choose...</option>
-                  <option>California</option>
-                </select>
-                <div className="invalid-feedback">
-                  Please provide a valid state.
-                </div>
-              </div>
-              <div className="col-md-3 mb-3">
-                <label for="zip">Zip</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="zip"
-                  placeholder=""
-                  required
-                />
-                <div className="invalid-feedback">Zip code required.</div>
-              </div>
-            </div>
-            <hr className="mb-4" />
-            <button className="btn btn-primary btn-lg btn-block" type="submit">
-              Continue to checkout
-            </button>
-          </form>
+          </div>
+          <div className="col-md-6 mb-3">
+            <label htmlFor="lastName">Last name</label>
+            <input
+              type="text"
+              className="form-control"
+              id="lastName"
+              placeholder="Doe"
+              value=""
+              required
+            />
+            <div className="invalid-feedback">Valid last name is required.</div>
+          </div>
         </div>
-      </div>
+        <div className="mb-3">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            placeholder="you@example.com"
+          />
+          <div className="invalid-feedback">
+            Please enter a valid email address for shipping updates.
+          </div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="address">Password</label>
+          <input
+            type="text"
+            className="form-control"
+            id="password"
+            placeholder="Password"
+            required
+          />
+          <div className="invalid-feedback">
+            Please enter your shipping address.
+          </div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="address">
+            Password<span className="text-muted"> (Re-enter)</span>
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="password"
+            placeholder="Password"
+            required
+          />
+          <div className="invalid-feedback">
+            Please enter your shipping address.
+          </div>
+        </div>
+        <hr className="mb-4" />
+        <button className="btn btn-primary btn-lg btn-block" type="submit">
+          Register
+        </button>
+      </form>
     </div>
   )
 }
