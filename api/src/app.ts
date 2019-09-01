807987d507343cd146e77eee39c25fd3849344b7
Thilo Ilg <contact@thiloilg.com>
2019-09-01 17:45:30 +0200

    chore(api): websocket with simple websocket example is working

--- api/src/app.ts
+++ api/src/app.ts
@@ -4,6 +4,8 @@ import bodyParser from 'koa-bodyparser'
 import cors from '@koa/cors'
 import session from 'koa-session'
 import serve from 'koa-static'
+import websocket from 'socket.io'
+import http from 'http'
 import { DataSupplyRouter } from './data-supply/data-supply.router'
 import { ApiDocsRouter } from './api-docs/api-docs.router'
 import { AdminRouter } from './admin/admin.router'
@@ -12,9 +14,13 @@ import { SessionRouter } from './session/session.router'
 
 export class App {
   private koa: Koa
+  private server: http.Server
+  private socket: websocket.Server
 
   public constructor() {
     this.koa = new Koa()
+    this.server = http.createServer(this.koa.callback())
+    this.socket = websocket(this.server)
 
     /**
      * prevent checking for https encryption behind reverse proxy
@@ -32,9 +38,13 @@ export class App {
     this.koa.use(new AdminRouter('/admin').routes())
     this.koa.use(new UserRouter('/users').routes())
     this.koa.use(new SessionRouter('/sessions').routes())
+
+    this.socket.on('connection', socket => {
+      console.log('Client has connected with socket id: ' + socket.id)
+    })
   }
 
   public listen(port: number): void {
-    this.koa.listen(port)
+    this.server.listen(port)
   }
 }
