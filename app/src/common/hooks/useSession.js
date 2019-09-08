0748875cd5d8180433fb0eb849739a940c5f7a22
Thilo Ilg <contact@thiloilg.com>
2019-09-08 15:30:19 +0200

    feat(app): provide decent queue for toasts

--- app/src/common/hooks/useSession.js
+++ app/src/common/hooks/useSession.js
@@ -3,10 +3,12 @@ import { useApiEndpoint } from './useApiEndpoint'
 import { API_URL } from '../../config'
 import { AppContext, INITIAL_APP_STATE } from '../context/AppContext'
 import statusCode from 'http-status-codes'
+import { ToastContext } from '../context/ToastContext'
 
 export function useSession() {
   const [open, , , close] = useApiEndpoint(`${API_URL}/sessions`)
   const [appState, setAppState] = useContext(AppContext)
+  const [, showToast] = useContext(ToastContext)
 
   async function login(email, password) {
     const [status, data] = await open({ email, password })
@@ -17,6 +19,10 @@ export function useSession() {
         firstName: data.firstName,
         lastName: data.lastName,
       })
+      showToast('1', 'nais')
+      setTimeout(() => {
+        showToast('2', 'strapped')
+      }, 3000)
     }
   }
 
@@ -24,6 +30,10 @@ export function useSession() {
     const [status] = await close()
     if (status === statusCode.NO_CONTENT) {
       setAppState(INITIAL_APP_STATE)
+      showToast('3', 'sheeesh')
+      setTimeout(() => {
+        showToast('4', 'bows')
+      }, 3000)
     }
   }
 
