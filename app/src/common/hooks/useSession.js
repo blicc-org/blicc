903b322eff00862fba61c6d22b12614f8783bb94
Thilo Ilg <contact@thiloilg.com>
2019-09-08 20:41:51 +0200

    feat(app): improve error logs in the frontend

--- app/src/common/hooks/useSession.js
+++ app/src/common/hooks/useSession.js
@@ -23,7 +23,7 @@ export function useSession() {
     } else if (status === statusCode.FORBIDDEN) {
       showToast('Wrong password', 'Please try it again.')
     } else if (status === statusCode.NOT_FOUND) {
-      showToast('Wrong email adresse', 'An account with the given email could not be found.')
+      showToast('Wrong email', 'No account for the given email.')
     } else {
       showToast('Login error', 'Login process failed.')
     }
