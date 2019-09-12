ae699ae4d6bba6bc3a17abae3df96fafe1c03edd
Thilo Ilg <contact@thiloilg.com>
2019-09-12 21:04:30 +0200

    feat(app): fix svg react import

--- app/src/pages/landing-page/Headline.jsx
+++ app/src/pages/landing-page/Headline.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { content } from '../../language/Content'
-import maskot from '../../assets/images/maskot.svg'
+import { ReactComponent as Maskot } from '../../assets/images/maskot.svg'
 
 export function Headline() {
   return (
     <div className="col-md-5 mx-auto py-5 my-5 text-center">
-      <img src={maskot} height={'100px'} alt="maskot" />
+      <Maskot height={'100px'} alt="maskot" />
       <h1 className="display-4 font-weight-normal">{content.metadata.title}</h1>
       <p className="lead font-weight-normal">{content.metadata.description}</p>
       <Link className="btn btn-outline-secondary" to="/dashboards">
