6eaf5e56404859a1ea26e90a4791a1f184199a65
Thilo Ilg <contact@thiloilg.com>
2019-09-17 22:06:18 +0200

    feat(delivery): update ports

--- app/src/config.js
+++ app/src/config.js
@@ -2,3 +2,6 @@ const isDev = process.env.NODE_ENV === 'development'
 
 export const API_URL = isDev ? 'http://localhost' : 'https://api.blicc.org'
 export const APP_URL = isDev ? 'http://localhost:3000' : 'https://blicc.org'
+export const DELIVERY_URL = isDev
+  ? 'http://localhost:8080'
+  : 'https://delivery.blicc.org'
