e5e9810c9c79f52c2e9c946c9857fa014132ed17
Thilo Ilg <contact@thiloilg.com>
2019-09-21 19:13:29 +0200

    refactor(all): gitlab ci

--- api/src/config.ts
+++ api/src/config.ts
@@ -3,6 +3,8 @@ const isDev = process.env.NODE_ENV === 'development'
 export const DOMAIN = isDev ? '127.0.0.1' : 'blicc.org'
 export const IS_PROD = !isDev
 
+export const PORT = 3001
+
 export const MAIL_ADDRESS = process.env.MAIL_ADDRESS
 export const MAIL_PASSWORD = process.env.MAIL_PASSWORD
 export const MAIL_HOST = process.env.MAIL_HOST
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,11 +1,12 @@
 import { App } from './app'
 import { createConnection } from 'typeorm'
+import { PORT } from './config'
 
 createConnection()
   .catch((e): void => {
     console.log(e)
   })
   .then((): void => {
-    new App().listen(80)
-    console.log(`The server is listening...`)
+    new App().listen(PORT)
+    console.log(`The server is listening on port ${PORT} ...`)
   })
