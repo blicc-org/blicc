e35b51df6e0a629acad1bbcf35559bd946ee7b25
Thilo Ilg <contact@thiloilg.com>
2019-09-22 17:53:28 +0200

    feat(delivery): go setup

---
+++ delivery/cmd/supplier/main.go
@@ -0,0 +1,19 @@
+package main
+
+// "github.com/gorilla/websocket"
+// "github.com/swaggest/go-asyncapi/spec"
+// "github.com/swaggest/go-asyncapi/swgen/asyncapi"
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	const PORT = 80
+
+	fmt.Printf("Starting server on port %d ...", PORT)
+
+	// supplier.NewService()
+
+}
--- delivery/go.mod
+++ delivery/go.mod
@@ -2,8 +2,4 @@ module delivery
 
 go 1.13
 
-require (
-	github.com/gorilla/websocket v1.4.1
-	github.com/sirupsen/logrus v1.4.2
-	github.com/swaggest/go-asyncapi v0.4.1 // indirect
-)
+require github.com/gorilla/websocket v1.4.1 // indirect
