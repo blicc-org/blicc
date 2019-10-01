e01c93ee1ad9a360429c9eef464d99f9b38d341d
Thilo Ilg <contact@thiloilg.com>
2019-10-01 22:07:40 +0200

    feat(app): websocket connection setup

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -8,26 +8,26 @@ import { App } from './App'
 
 import * as serviceWorker from './serviceWorker'
 
-// import { DELIVERY_URL } from './config'
+import { DELIVERY_URL } from './config'
 
-// let socket = new WebSocket(DELIVERY_URL)
+let socket = new WebSocket(DELIVERY_URL)
 
-// console.log('waiting for websocket connection')
-// socket.onopen = () => {
-//   socket.send('Hi from client')
+console.log('waiting for websocket connection')
+socket.onopen = () => {
+  socket.send('Hi from client')
 
-//   socket.onmessage = ({ data }) => {
-//     console.log(`Socket received message: ${data}`)
-//   }
+  socket.onmessage = ({ data }) => {
+    console.log(`Socket received message: ${data}`)
+  }
 
-//   socket.onclose = event => {
-//     console.log(`Socket closed connection: ${event}`)
-//   }
+  socket.onclose = event => {
+    console.log(`Socket closed connection: ${event}`)
+  }
 
-//   socket.onerror = event => {
-//     console.log(`Socket error occured: ${event}`)
-//   }
-// }
+  socket.onerror = event => {
+    console.log(`Socket error occured: ${event}`)
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
