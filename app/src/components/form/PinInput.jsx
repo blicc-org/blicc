1d848922590ca833914c41b3b4ccd706b609239d
Thilo Ilg <contact@thiloilg.com>
2019-10-05 18:32:55 +0200

    feat(app): concatenate digits to pin

--- app/src/components/form/PinInput.jsx
+++ app/src/components/form/PinInput.jsx
@@ -1,30 +1,57 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
+import './PinInput.scss'
 
-export function Pin({ num, setNum }) {
+export function Digit({ digit, setDigit, isFocused, setPos }) {
+  const ref = useRef(null)
+  useEffect(() => {
+    if (isFocused) ref.current.focus()
+  })
+
   return (
     <input
-      id="inlineFormInputGroupUsername"
       className="form-control"
       type="text"
-      pattern="[0-9]*"
-      onChange={e => setNum(e.target.validity.valid ? e.target.value : num)}
-      value={num}
-      maxLength="1"
-      autocomplete="off"
+      ref={ref}
+      value={digit}
+      onChange={({ target: t }) => {
+        const input = t.value.charAt(t.selectionStart - 1)
+        if (/^(?:[0-9])$/.test(input) || input === '') {
+          setDigit(input)
+        }
+      }}
+      autoComplete="off"
       required
     />
   )
 }
 
-export function PinInput({ pin, setPin }) {
-  const init = new Array(6)
-  init.fill('')
-  const [array, setArray] = useState(init)
-
+export function PinInput({}) {
+  const [pos, setPos] = useState(0)
+  const [pin, setPin] = useState({
+    0: '',
+    1: '',
+    2: '',
+    3: '',
+    4: '',
+    5: '',
+  })
+  
   return (
-    <div className="input-group mb-3">
-      {array.map(() => {
-        return <Pin pos={1} num={array} setNum={setArray} />
+    <div className="input-group mb-3 pin-input">
+      {Object.keys(pin).map((key, index) => {
+        return (
+          <Digit
+            key={key}
+            digit={pin[key]}
+            setDigit={value => {
+              setPin(prev => {
+                return { ...prev, [key]: value }
+              })
+            }}
+            setPos={setPos}
+            isFocused={index === pos}
+          />
+        )
       })}
     </div>
   )
