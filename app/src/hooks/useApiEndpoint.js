504d1bc5c495a04059609f9f6dbb5334ea3b2684
Thilo Ilg <contact@thiloilg.com>
2019-10-06 09:48:48 +0200

    refactor(app): remove try catch from api wrapper

--- app/src/hooks/useApiEndpoint.js
+++ app/src/hooks/useApiEndpoint.js
@@ -4,39 +4,23 @@ export function useApiEndpoint(path) {
   const config = { withCredentials: true }
 
   async function create(resource) {
-    try {
-      const { data, status } = await axios.post(path, resource, config)
-      return [status, data]
-    } catch (e) {
-      throw e
-    }
+    const { data, status } = await axios.post(path, resource, config)
+    return [status, data]
   }
 
   async function access() {
-    try {
-      const { data, status } = await axios.get(path, config)
-      return [status, data]
-    } catch (e) {
-      throw e
-    }
+    const { data, status } = await axios.get(path, config)
+    return [status, data]
   }
 
   async function update(resource) {
-    try {
-      const { data, status } = await axios.put(path, resource, config)
-      return [status, data]
-    } catch (e) {
-      throw e
-    }
+    const { data, status } = await axios.put(path, resource, config)
+    return [status, data]
   }
 
   async function remove() {
-    try {
-      const { data, status } = await axios.delete(path, config)
-      return [status, data]
-    } catch (e) {
-      throw e
-    }
+    const { data, status } = await axios.delete(path, config)
+    return [status, data]
   }
 
   return [create, access, update, remove]
