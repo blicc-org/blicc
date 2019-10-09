e73d3ac32536b60e629d41f2ff95f47509015516
Thilo Ilg <contact@thiloilg.com>
2019-10-09 11:27:31 +0200

    chore(all): remove create certs from ci file

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+# file for traefik to store certificates
 touch acme.json
 chmod 600 acme.json
+
+# rsa keys for app authorization
+openssl genrsa -out rsa.pem 2048
+openssl rsa -in rsa.pem -outform PEM -pubout -out rsa_pub.pem
