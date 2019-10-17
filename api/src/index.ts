4047044a8ef550cd70ced13e1a66e21881f4d631
Thilo Ilg <contact@thiloilg.com>
2019-10-17 07:41:23 +0200

    feat(api): pass port as command line argument

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,12 +1,20 @@
+import flags from 'commander'
 import { App } from './app'
 import { createConnection } from 'typeorm'
 import { PORT } from './config'
+
+flags
+  .option('-p, --port <number>', 'Port the server listens on')
+  .option('-c, --certificate-path <string>', 'Path to certificates')
+
+flags.parse(process.argv);
 
 createConnection()
   .catch((e): void => {
     console.log(e)
   })
   .then((): void => {
-    new App().listen(PORT)
-    console.log(`The server is listening on port ${PORT} ...`)
+    const port = flags.port ? flags.port : PORT
+    new App().listen(port)
+    console.log(`The server is listening on port ${port} ...`)
   })
