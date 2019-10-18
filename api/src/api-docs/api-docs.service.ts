0403f8f61df1cd7c3ef1b4bfac9ca876d92524ba
Thilo Ilg <contact@thiloilg.com>
2019-10-18 13:51:18 +0200

    chore(all): major v0.0.3 upgrade

--- api/src/api-docs/api-docs.service.ts
+++ api/src/api-docs/api-docs.service.ts
@@ -7,7 +7,7 @@ export class ApiDocsService {
       openapi: '3.0.0',
       info: {
         title: 'api.blicc.org',
-        version: '0.0.2',
+        version: '0.0.3',
         description: 'Backend api documentation',
       },
       components: {
