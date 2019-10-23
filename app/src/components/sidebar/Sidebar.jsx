c76d138c8f79b62ce849dac486517ec6fb2b4173
Thilo Ilg <contact@thiloilg.com>
2019-10-23 08:16:19 +0200

    feat(app): display specific sidebar content only if user is logged in

--- app/src/components/sidebar/Sidebar.jsx
+++ app/src/components/sidebar/Sidebar.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useContext } from 'react'
+import { AppContext } from '../../context/AppContext'
 import { Link } from 'react-router-dom'
 import { Home, File, ShoppingCart, Heart } from 'react-feather'
 import { SidebarContext } from '../../context/SidebarContext'
@@ -16,6 +17,8 @@ function SidebarHeader({ name }) {
 }
 
 export function Sidebar({ open }) {
+  const [appState] = useContext(AppContext)
+  const { loggedIn } = appState
   const [, setSidebarState] = useContext(SidebarContext)
   const [sidebarStyle, setSidebarStyle] = useState({})
   const [blackoutStyle, setBlackoutStyle] = useState({})
@@ -42,42 +45,61 @@ export function Sidebar({ open }) {
   return (
     <>
       <div className="sidebar" style={sidebarStyle}>
-        <SidebarHeader name="Dashboards" />
-        <ul className="nav flex-column px-2">
-          <li className="nav-item">
-            <Link className="nav-link active" to="/dashboards" onClick={close}>
-              <Home className="feather" /> Dashboard
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/test" onClick={close}>
-              <Heart className="feather" /> Test
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/" onClick={close}>
-              <File className="feather" /> Orders
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/" onClick={close}>
-              <ShoppingCart className="feather" /> Products
-            </Link>
-          </li>
-        </ul>
-        <SidebarHeader name="Charts" />
-        <ul className="nav flex-column px-2">
-          <li className="nav-item">
-            <Selector id={CHART_TYPE.lineChart} onDragStart={close} />
-          </li>
-          <li className="nav-item">
-            <Selector id={CHART_TYPE.barChart} onDragStart={close} />
-          </li>
-          <li className="nav-item">
-            <Selector id={CHART_TYPE.pieChart} onDragStart={close} />
-          </li>
-        </ul>
-        <SidebarHeader name="Data" />
+        {loggedIn ? (
+          <>
+            <SidebarHeader name="Dashboards" />
+            <ul className="nav flex-column px-2">
+              <li className="nav-item">
+                <Link
+                  className="nav-link active"
+                  to="/dashboards"
+                  onClick={close}
+                >
+                  <Home className="feather" /> Dashboard
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/test" onClick={close}>
+                  <Heart className="feather" /> Test
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/" onClick={close}>
+                  <File className="feather" /> Orders
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/" onClick={close}>
+                  <ShoppingCart className="feather" /> Products
+                </Link>
+              </li>
+            </ul>
+            <SidebarHeader name="Charts" />
+            <ul className="nav flex-column px-2">
+              <li className="nav-item">
+                <Selector id={CHART_TYPE.lineChart} onDragStart={close} />
+              </li>
+              <li className="nav-item">
+                <Selector id={CHART_TYPE.barChart} onDragStart={close} />
+              </li>
+              <li className="nav-item">
+                <Selector id={CHART_TYPE.pieChart} onDragStart={close} />
+              </li>
+            </ul>
+            <SidebarHeader name="Data" />
+          </>
+        ) : (
+          <>
+            <ul className="nav flex-column px-2 pt-4">
+              <li className="nav-item">
+                <p className="text-muted">
+                  <Link to="/register">Register now</Link>
+                  {`  to start designing and analysing your data.`}
+                </p>
+              </li>
+            </ul>
+          </>
+        )}
       </div>
       <div className="blackout" onClick={close} style={blackoutStyle}></div>
     </>
