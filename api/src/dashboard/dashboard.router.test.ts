51aef8f722bc891b1079d7b307d0ad81e72da076
Thilo Ilg <contact@thiloilg.com>
2019-10-24 11:35:49 +0200

    feat(api): routes test structure for /dashboard tests

---
+++ api/src/dashboard/dashboard.router.test.ts
@@ -0,0 +1,19 @@
+describe('GET: /dashboards', () => {
+  it('200: OK', () => {})
+})
+
+describe('GET: /dashboards/:id', () => {
+  it('200: OK', () => {})
+})
+
+describe('POST: /dashboards', () => {
+  it('201: Created', () => {})
+})
+
+describe('PUT: /dashboards/:id', () => {
+  it('200: OK', () => {})
+})
+
+describe('DELETE: /dashboards/:id', () => {
+  it('200: OK', () => {})
+})
