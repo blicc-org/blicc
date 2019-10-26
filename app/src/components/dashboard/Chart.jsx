41a21494981b394896d1729a8a0be0da17d40e3b
Thilo Ilg <contact@thiloilg.com>
2019-10-26 15:02:03 +0200

    feat(app): move positioning in seperate component

--- app/src/components/dashboard/Chart.jsx
+++ app/src/components/dashboard/Chart.jsx
@@ -1,90 +1,15 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState } from 'react'
+import { Positioning } from './Positioning'
 import './Chart.scss'
 
 export function Chart({ type, id }) {
   const color = '#' + ((Math.random() * 0xffffff) << 0).toString(16)
-  // 0: none, 1: top, 2: right, 3: bottom, 4: left, 5: middle
-  const [sector, setSector] = useState(0)
-  const canvasRef = useRef(null)
-
-  useEffect(() => {
-    const ctx = canvasRef.current.getContext('2d')
-    const scale = window.devicePixelRatio
-    const width = canvasRef.current.offsetWidth * scale
-    const height = canvasRef.current.offsetHeight * scale
-    canvasRef.current.width = width
-    canvasRef.current.height = height
-
-    ctx.lineWidth = scale
-    ctx.beginPath()
-    ctx.moveTo(0, 0)
-    ctx.lineTo(width, height)
-    ctx.stroke()
-    ctx.beginPath()
-    ctx.moveTo(0, height)
-    ctx.lineTo(width, 0)
-    ctx.stroke()
-    ctx.strokeRect(width / 4, height / 4, width / 2, height / 2)
-  })
-
-  function isCenter(x, y) {
-    return (x > 0.25 && x < 0.75) && (y > 0.25 && y < 0.75)
-  }
-
-  function getDirection(x, y) {
-    if (x > y) {
-      return x + y < 1 ? 1 : 2
-    } else {
-      return x + y >= 1 ? 3 : 4
-    }
-  }
-
-  function normalize(x, y, width, height) {
-    return [x / width, y / height]
-  }
-
-  function getCanvasCoordinates(clientX, clientY) {
-    const rect = canvasRef.current.getBoundingClientRect()
-    return [clientX - rect.left, clientY - rect.top]
-  }
 
   return (
     <div className="col-6 chart p-5" style={{ backgroundColor: color }}>
-      <canvas
-        ref={canvasRef}
-        style={{
-          width: '100%',
-          height: '100%',
-          display: 'transparent',
-          position: 'absolute',
-          top: 0,
-          right: 0,
-          bottom: 0,
-          left: 0,
-        }}
-        onDragEnter={() => setSector(0)}
-        onDragLeave={() => setSector(0)}
-        onDragOver={event => {
-          const [x, y] = getCanvasCoordinates(event.clientX, event.clientY)
-
-          const width = canvasRef.current.offsetWidth
-          const height = canvasRef.current.offsetHeight
-          const [normalizedX, normalizedY] = normalize(x, y, width, height)
-
-          setSector(
-            isCenter(normalizedX, normalizedY)
-              ? 5
-              : getDirection(normalizedX, normalizedY)
-          )
-        }}
-        onDrop={event => {
-          event.preventDefault()
-          console.log(`You dropped on ${id}`)
-        }}
-      ></canvas>
+      <Positioning onDrop={num => console.log(num)} />
       <h3>{type}</h3>
       <h5>{id}</h5>
-      <h6>{sector.toString()}</h6>
     </div>
   )
 }
