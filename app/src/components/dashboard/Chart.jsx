f6f53ad7c2e67ebb2c784d97a7c59e562d509db7
Thilo Ilg <contact@thiloilg.com>
2019-10-26 18:41:14 +0200

    refactor(app): simplify iteration over charts

--- app/src/components/dashboard/Chart.jsx
+++ app/src/components/dashboard/Chart.jsx
@@ -4,9 +4,8 @@ import './Chart.scss'
 
 export function Chart({ type, id, onDrop }) {
   const color = '#' + ((Math.random() * 0xffffff) << 0).toString(16)
-
   return (
-    <div className="col chart" key={id} style={{ backgroundColor: color }}>
+    <div className="chart" key={id} style={{ backgroundColor: color }}>
       <Positioning onDrop={onDrop} />
       <h3>{type}</h3>
       <h5>{id}</h5>
--- app/src/components/dashboard/Dashboard.jsx
+++ app/src/components/dashboard/Dashboard.jsx
@@ -1,42 +1,40 @@
 import React, { useState } from 'react'
-import uuid from 'uuid'
 import { Chart } from './Chart'
 import { init } from './arrangement'
 import './Dashboard.scss'
 
-export function Dashboard() {
-  const [arrangement] = useState(init)
-
-  function onDropHandler(id, pos, type) {
-    console.log('id: ', id, ', pos: ', pos, ', type: ', type)
-  }
-
-  function Row({ row }) {
-    return (
-      <div className="row">
-        {row.map(obj => {
-          return obj.row ? (
-            <div className="col" key={uuid()}>
-              <Row row={obj.row} />
-            </div>
+function Row({ row, onDrop }) {
+  return (
+    <div className="row">
+      {row.map((obj, index) => (
+        <div key={index} className="col">
+          {obj.row ? (
+            <Row row={obj.row} />
           ) : (
             <Chart
               id={obj.id}
-              key={uuid()}
               type={obj.type}
-              onDrop={(sector, type) => onDropHandler(obj.id, sector, type)}
+              onDrop={(sector, type) => onDrop(obj.id, sector, type)}
             />
-          )
-        })}
-      </div>
-    )
-  }
+          )}
+        </div>
+      ))}
+    </div>
+  )
+}
 
+export function Dashboard() {
+  const [arrangement] = useState(init)
   return (
     <>
       <div className="dashboard" onDragOver={event => event.preventDefault()}>
         <div className="container">
-          <Row row={arrangement.row} />
+          <Row
+            row={arrangement.row}
+            onDrop={(id, pos, type) =>
+              console.log('id: ', id, ', pos: ', pos, ', type: ', type)
+            }
+          />
         </div>
       </div>
     </>
