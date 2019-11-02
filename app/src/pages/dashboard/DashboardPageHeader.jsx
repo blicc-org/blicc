84294cba8f478ec286e999a83a3a8b988e3c3729
Thilo Ilg <contact@thiloilg.com>
2019-11-02 08:56:43 +0100

    feat(app): add edit button to dashboard

--- app/src/pages/dashboard/DashboardPageHeader.jsx
+++ app/src/pages/dashboard/DashboardPageHeader.jsx
@@ -1,25 +1,14 @@
 import React from 'react'
-import { Calendar } from 'react-feather'
-import './DashboardPageHeader.scss'
+import { Edit } from 'react-feather'
 
 export function DashboardPageHeader() {
   return (
     <div className="dashboard-header d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
       <h1 className="h2 pr-3">Dashboard</h1>
       <div className="btn-toolbar mb-2 mb-md-0">
-        <div className="btn-group mr-2">
-          <button type="button" className="btn btn-sm btn-outline-secondary">
-            Share
-          </button>
-          <button type="button" className="btn btn-sm btn-outline-secondary">
-            Export
-          </button>
-        </div>
-        <button
-          type="button"
-          className="btn btn-sm btn-outline-secondary dropdown-toggle"
-        >
-          <Calendar className="feather" /> This week
+        <button type="button" className="btn btn-sm btn-outline-secondary">
+          <Edit className="mr-1" size={16} />
+          Edit
         </button>
       </div>
     </div>
