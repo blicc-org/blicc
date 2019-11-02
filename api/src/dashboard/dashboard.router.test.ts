4b856babf62170e27178f16b75f285f7025b1ea0
Thilo Ilg <contact@thiloilg.com>
2019-11-02 16:53:31 +0100

    feat(api): add swagger docs to create dashboard route

--- api/src/dashboard/dashboard.router.test.ts
+++ api/src/dashboard/dashboard.router.test.ts
@@ -1,23 +1,31 @@
 import { instance, initializeUser } from '../test/user.helper'
 
 describe('POST: /dashboards', () => {
+  const body = {
+    title: 'Dashboard Name',
+    data: {},
+  }
   let params = { email: '', userId: '', cookie: '' }
   beforeEach(async () => {
     params = await initializeUser()
   })
 
   it('201: Created', async () => {
-    const data = {
-      title: 'Dashboard Name',
-      data: {},
-    }
-
-    const response = await instance.post('/dashboards', data, {
+    const { status, data } = await instance.post('/dashboards', body, {
       headers: {
         Cookie: params.cookie,
       },
     })
-    expect(response.status).toBe(201)
+    expect(status).toBe(201)
+    expect(data.userId).toBe(params.userId)
+  })
+
+  it('401: Unauthorized', async () => {
+    let response = await instance.post('/dashboards', body, {})
+    expect(response.status).toBe(401)
+
+    response = await instance.post('/dashboards', { data: {} }, {})
+    expect(response.status).toBe(401)
   })
 })
 
--- api/src/dashboard/dashboard.router.ts
+++ api/src/dashboard/dashboard.router.ts
@@ -23,6 +23,89 @@ export class DashboardRouter {
       PermissionMiddleware.handle.bind(null, ['user', 'admin'])
     )
 
+    /**
+     * @swagger
+     *
+     * /dashboards:
+     *   post:
+     *     security:
+     *       - cookieAuth: []
+     *     securitySchemes:
+     *       bearerAuth:
+     *         type: http
+     *         scheme: bearer
+     *         bearerFormat: JWT
+     *     produces:
+     *       - application/json
+     *     tags:
+     *       - Dashboard
+     *     summary: Create dashboard
+     *     description: create a dasbhoard
+     *     requestBody:
+     *         content:
+     *           application/json:
+     *             schema:
+     *               required:
+     *               - dashboard
+     *               properties:
+     *                 dashboard:
+     *                   type: object
+     *                   required:
+     *                   - title
+     *                   - data
+     *                   properties:
+     *                     title:
+     *                       type: string
+     *                     data:
+     *                       type: json
+     *             examples:
+     *               filter:
+     *                 value: {
+     *                   "title": "Dashboard",
+     *                   "data": "{}",
+     *                 }
+     *     responses:
+     *       201:
+     *         description: Created
+     *         content:
+     *           application/json:
+     *             schema:
+     *               required:
+     *               - dashboard
+     *               properties:
+     *                 dashboard:
+     *                   type: object
+     *                   required:
+     *                   - id
+     *                   - title
+     *                   - userId
+     *                   - data
+     *                   - creationDate
+     *                   properties:
+     *                     id:
+     *                       type: string
+     *                     title:
+     *                       type: string
+     *                     userId:
+     *                       type: string
+     *                     data:
+     *                       type: json
+     *                     creationDate:
+     *                       type: string
+     *             examples:
+     *               filter:
+     *                 value: {
+     *                   id: "WaDQc9_H",
+     *                   title: "Dashboard",
+     *                   userId: "b1x_S29n",
+     *                   data: {},
+     *                   creationDate: "2019-11-02T15:45:58.284Z"
+     *                 }
+     *       401:
+     *         description: Unauthorized
+     *       500:
+     *         description: Internal Server Error
+     */
     this.router.route({
       method: 'post',
       path: '/',
