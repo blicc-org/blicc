608339e9563964fb1d334f0d2e2911247dc1e7d1
Thilo Ilg <contact@thiloilg.com>
2019-11-03 20:54:34 +0100

    feat(app): remove jquery

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -1,6 +1,4 @@
 import './assets/scss/Bootstrap.scss'
-import 'bootstrap/dist/js/bootstrap.min.js'
-import './assets/js/drag-drop-polyfill.js'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
