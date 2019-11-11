2d72addd5e9e39713a788425b481d2399c7c5fbe
Thilo Ilg <contact@thiloilg.com>
2019-11-11 20:21:01 +0100

    feat(app): add select language in footer

--- app/src/components/footer/Footer.jsx
+++ app/src/components/footer/Footer.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FooterNav } from './FooterNav'
 import { content } from '../../content/content'
-import './Footer.scss'
 
 export function Footer() {
+  const languages = ['german', 'english']
+  const [language, setLanguage] = useState()
+
   return (
     <footer>
       <FooterNav />
-      <div className="col-12 col-md">
+      <div className="m-3">
+        <select className="form-control form-control-sm">
+          {languages.map(lg => {
+            return <option key={lg}>{lg}</option>
+          })}
+        </select>
+      </div>
+      <div className="m-3">
         <h3>blicc.org</h3>
         <small className="d-block mb-3 text-muted">
           <p> {`${content.en.copyright} ${new Date().getFullYear()}`}</p>
--- app/src/components/footer/FooterNavList.jsx
+++ app/src/components/footer/FooterNavList.jsx
@@ -3,7 +3,7 @@ import { FooterNavListItem } from './FooterNavListItem'
 
 export function FooterNavList({ list }) {
   return (
-    <div className="col-6 col-md">
+    <div className="m-3">
       <h5>{list.category}</h5>
       <ul className="list-unstyled text-small">
         {list.items.map(item => (
