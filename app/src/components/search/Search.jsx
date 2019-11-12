081877d555d99bc75795b2d87d0c345c1d5ba9ed
Thilo Ilg <contact@thiloilg.com>
2019-11-12 21:22:46 +0100

    feat(app): add search box for mobile

--- app/src/components/search/Search.jsx
+++ app/src/components/search/Search.jsx
@@ -1,21 +1,60 @@
-import React from 'react'
-import { Search as SearchIcon } from 'react-feather'
+import React, { useState } from 'react'
+import { Search as SearchIcon, ArrowLeft } from 'react-feather'
+import { useMobile } from '../../hooks/useMobile'
+import theme from '../../Theme.scss'
 import './Search.scss'
 
 export function Search() {
+  const isMobile = useMobile()
+  const [open, setOpen] = useState(false)
+
   return (
-    <form className="form-inline input-group search w-100 pr-5">
-      <input
-        className="form-control"
-        type="search"
-        placeholder="Search"
-        aria-label="Search"
-      />
-      <div className="input-group-append">
-        <button className="btn" type="submit">
-          <SearchIcon className="search-icon" />
-        </button>
-      </div>
-    </form>
+    <>
+      {open && (
+        <div className="mobile-search">
+          <form className="form-inline input-group search w-100">
+            <div className="input-group-prepend">
+              <button className="btn" onClick={() => setOpen(false)}>
+                <ArrowLeft />
+              </button>
+            </div>
+            <input
+              className="form-control"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <div className="input-group-append">
+              <button className="btn" type="submit">
+                <SearchIcon />
+              </button>
+            </div>
+          </form>
+        </div>
+      )}
+      {isMobile ? (
+        <a
+          className="nav-link ml-auto"
+          style={{ cursor: 'pointer' }}
+          onClick={() => setOpen(true)}
+        >
+          <SearchIcon style={{ color: theme.lightgray }} />
+        </a>
+      ) : (
+        <form className="form-inline input-group search w-100 pr-5">
+          <input
+            className="form-control"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <div className="input-group-append">
+            <button className="btn" type="submit">
+              <SearchIcon />
+            </button>
+          </div>
+        </form>
+      )}
+    </>
   )
 }
