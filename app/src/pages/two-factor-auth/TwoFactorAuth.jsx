dcd1e385c4768bde90c89bdc678f81ce5a3864e9
Thilo Ilg <contact@thiloilg.com>
2019-11-15 15:50:02 +0100

    refactor(app): clear up two factor auth

--- app/src/pages/two-factor-auth/TwoFactorAuth.jsx
+++ app/src/pages/two-factor-auth/TwoFactorAuth.jsx
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from 'react'
-import { TwoFactorAuthForm } from './TwoFactorAuthForm'
 import { useApiEndpoint } from '../../hooks/useApiEndpoint'
+import { Redirect } from 'react-router-dom'
+import { QRCode } from '../../components/qr-code/QRCode'
+import { Pin } from '../../components/form/pin/Pin'
+import './TwoFactorAuth.scss'
 
 export function TwoFactorAuth() {
   const [enable, requestSecret, ,] = useApiEndpoint('/two-factor-auth')
   const [url, setUrl] = useState('')
+  const [token, setToken] = useState('')
+  const [onSuccess, setOnSuccess] = useState(false)
 
   useEffect(() => {
     async function fetchChallengeUrl() {
@@ -15,5 +20,35 @@ export function TwoFactorAuth() {
     // eslint-disable-next-line
   }, [])
 
-  return <TwoFactorAuthForm url={url} enable={enable} />
+  return (
+    <>
+      {' '}
+      {onSuccess && <Redirect to="/dashboards" />}
+      <div className="col-md-5 mx-auto py-5 my-5 text-center">
+        <form className="form-two-factor-auth">
+          <h1 className="h3 mb-3 font-weight-normal">Two-factor auth</h1>
+          <QRCode url={url}></QRCode>
+          <div className="checkbox mb-3">
+            Scan the QR Code with your favorite two-factor authentication app
+            like Authy, Google Authenticator etc.
+          </div>
+          <label htmlFor="token" className="sr-only">
+            Token
+          </label>
+          <Pin size={6} setPin={setToken} />
+          <button
+            className="btn btn-lg btn-primary btn-block"
+            type="submit"
+            onClick={async e => {
+              e.preventDefault()
+              await enable({ token })
+              setOnSuccess(true)
+            }}
+          >
+            Activate
+          </button>
+        </form>
+      </div>
+    </>
+  )
 }
