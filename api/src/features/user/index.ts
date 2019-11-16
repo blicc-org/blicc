b641b5f370bd6f4adbf986381d69a41b730c78c3
Thilo Ilg <contact@thiloilg.com>
2019-11-16 20:53:37 +0100

    refactor(api): add index file to user

--- api/src/features/user/index.ts
+++ api/src/features/user/index.ts
@@ -1,4 +1,4 @@
 export { UserRouter } from './user.router'
 export { UserService } from './user.service'
-export { User} from './user.interface'
-export { UserEntity} from './user.entity'
+export { User } from './user.interface'
+export { UserEntity } from './user.entity'
