c9d140f6c55e76e9e8d9e891db140216913be295
Thilo Ilg <contact@thiloilg.com>
2019-11-23 14:36:10 +0100

    feat(app): add toolbox design

--- app/src/common/components/toolbox/Toolbox.jsx
+++ app/src/common/components/toolbox/Toolbox.jsx
@@ -1,40 +1,41 @@
 import React, { useState, useRef } from 'react'
 import { useClickAway } from '../../hooks'
-import { Plus } from 'react-feather'
+import { Plus, PieChart, Database } from 'react-feather'
 import './Toolbox.scss'
 
 export function Toolbox() {
   const [show, setShow] = useState(false)
   const ref = useRef()
   useClickAway(ref, () => setShow(false), 'prevent-toolnox-click-away')
+  let rotation = show ? -135 : 0
 
   return (
     <>
-      <div className="dropdown dropup toolbox">
+      <div className="toolbox" ref={ref}>
+        {show && (
+          <>
+            <div className="draggable" draggable={true}>
+              <PieChart />
+            </div>
+            <div className="draggable" draggable={true}>
+              <Database />
+            </div>
+          </>
+        )}
         <button
-          className="btn btn-primary prevent-toolnox-click-away"
+          className={`btn btn-primary prevent-toolnox-click-away ${
+            show ? 'show' : ''
+          }`}
           type="button"
           id="dropdownMenuButton"
           data-toggle="dropdown"
           aria-haspopup="true"
           aria-expanded="false"
           onClick={() => setShow(!show)}
+          style={{ transform: `rotate(${rotation}deg)` }}
         >
           <Plus />
         </button>
-        <div
-          ref={ref}
-          className={`dropdown-menu dropdown-menu-right ${show ? 'show' : ''}`}
-          aria-labelledby="dropdownMenuButton"
-        >
-          <h6 className="dropdown-header">Edit Dashboard</h6>
-          <a className="dropdown-item" href="/">
-            Chart
-          </a>
-          <a className="dropdown-item" href="/">
-            Data Relation
-          </a>
-        </div>
       </div>
     </>
   )
