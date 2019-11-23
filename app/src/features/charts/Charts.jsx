46ef83bf7c88d5873d0823f3ab7a00cdbcf85b77
Thilo Ilg <contact@thiloilg.com>
2019-11-23 19:13:46 +0100

    feat(app): add list of chart types

--- app/src/features/charts/Charts.jsx
+++ app/src/features/charts/Charts.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
+import { ChartsItem } from './ChartsItem'
 
 export function Charts() {
+  const plugins = ['@features/pie-chart', '@features/line-chart']
+
   return (
     <>
       <div className="container">
         <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center my-3">
           <h2 className="my-0">Charts</h2>
+        </div>
+
+        <div className="chart-list">
+          {plugins.length === 0 ? (
+            <p className="text-muted mx-auto py-5 my-5 text-center">
+              No charts fount.
+            </p>
+          ) : (
+            <table className="table">
+              <tbody>
+                {plugins.map(plugin => {
+                  return (
+                    <ChartsItem key={plugin} path={plugin}>
+                      {plugin}
+                    </ChartsItem>
+                  )
+                })}
+              </tbody>
+            </table>
+          )}
         </div>
       </div>
     </>
---
+++ app/src/features/charts/ChartsItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export function ChartsItem({ path }) {
+  return (
+    <tr>
+      <td>
+        <h5 className="card-title">{path}</h5>
+      </td>
+    </tr>
+  )
+}
