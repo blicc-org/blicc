35ee92d42c2cc8a93c26b1fe7b138233305a0824
Thilo Ilg <contact@thiloilg.com>
2019-12-02 17:47:13 +0100

    feat(app): list charts

--- app/src/features/charts/Charts.jsx
+++ app/src/features/charts/Charts.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import statusCode from 'http-status-codes'
 import { ChartsItem } from './ChartsItem'
+import { useApiEndpoint } from '../../common/hooks'
 
 export function Charts() {
-  const plugins = ['@essentials/pie-chart', '@essentials/line-chart']
+  const [result, setResult] = useState({ total: 0, charts: [] })
+  const [, access, ,] = useApiEndpoint('/charts')
+
+  useEffect(() => {
+    async function fetchData() {
+      const [status, data] = await access()
+      if (status === statusCode.OK) {
+        setResult(data)
+      }
+    }
+    fetchData()
+    // eslint-disable-next-line
+  }, [])
 
   return (
     <>
@@ -10,21 +24,16 @@ export function Charts() {
         <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center my-3">
           <h2 className="my-0">Charts</h2>
         </div>
-
         <div className="chart-list">
-          {plugins.length === 0 ? (
+          {result.total === 0 ? (
             <p className="text-muted mx-auto py-5 my-5 text-center">
               No charts fount.
             </p>
           ) : (
             <table className="table">
               <tbody>
-                {plugins.map(plugin => {
-                  return (
-                    <ChartsItem key={plugin} path={plugin}>
-                      {plugin}
-                    </ChartsItem>
-                  )
+                {result.charts.map(chart => {
+                  return <ChartsItem key={chart.id} chart={chart} />
                 })}
               </tbody>
             </table>
--- app/src/features/charts/ChartsItem.jsx
+++ app/src/features/charts/ChartsItem.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
-export function ChartsItem({ path }) {
+export function ChartsItem({ chart }) {
   return (
     <tr>
       <td>
-        <h5 className="card-title">{path}</h5>
+        <h5 className="card-title">{chart.title}</h5>
+        <p>{chart.id}</p>
+        <p>{chart.bundle}</p>
+        <p>{chart.description}</p>
+        <p>{chart.creationDate}</p>
+        <p>{chart.slug}</p>
       </td>
     </tr>
   )
