92f241185cc5149f1e1ad95fc1d8d5502d653719
Thilo Ilg <contact@thiloilg.com>
2019-12-07 00:28:26 +0100

    feat(api): add documentation for data source

--- api/src/features/data-source/data-source.router.ts
+++ api/src/features/data-source/data-source.router.ts
@@ -20,6 +20,112 @@ export class DataSourceRouter {
     this.router.prefix(this.prefix)
     this.router.use(bodyParser({ strict: true }))
 
+    /**
+     * @swagger
+     *
+     * /data-source:
+     *   post:
+     *     security:
+     *       - cookieAuth: []
+     *     securitySchemes:
+     *       bearerAuth:
+     *         type: http
+     *         scheme: bearer
+     *         bearerFormat: JWT
+     *     produces:
+     *       - application/json
+     *     tags:
+     *       - Data Source
+     *     summary: Create data source
+     *     description: Create a new data source.
+     *     requestBody:
+     *         content:
+     *           application/json:
+     *             schema:
+     *               required:
+     *               - data-source
+     *               properties:
+     *                 data-source:
+     *                   type: object
+     *                   required:
+     *                   - title
+     *                   - requestConfig
+     *                   - persistData
+     *                   - fetchFrequency
+     *                   properties:
+     *                     title:
+     *                       type: string
+     *                     description:
+     *                       type: string
+     *                     requestConfig:
+     *                       type: json
+     *                     persistData:
+     *                       type: boolean
+     *                     fetchFrequency:
+     *                       type: number
+     *             examples:
+     *               filter:
+     *                 value: {
+     *                   title: "Api",
+     *                   description: "Fetch data from a Api",
+     *                   requestConfig: {},
+     *                   persistData: false,
+     *                   fetchFrequency: 86400000
+     *                 }
+     *     responses:
+     *       201:
+     *         description: Created
+     *         content:
+     *           application/json:
+     *             schema:
+     *               required:
+     *               - data-source
+     *               properties:
+     *                 data-source:
+     *                   type: object
+     *                   required:
+     *                   - id
+     *                   - title
+     *                   - description
+     *                   - userId
+     *                   - requestConfig
+     *                   - creationDate
+     *                   - persistData
+     *                   - fetchFrequency
+     *                   properties:
+     *                     id:
+     *                       type: string
+     *                     title:
+     *                       type: string
+     *                     description:
+     *                       type: string
+     *                     userId:
+     *                       type: string
+     *                     requestConfig:
+     *                       type: json
+     *                     creationDate:
+     *                       type: string
+     *                     persistData:
+     *                       type: boolean
+     *                     fetchFrequency:
+     *                       type: number
+     *             examples:
+     *               filter:
+     *                 value: {
+     *                   id: "3cT4lb0M",
+     *                   title: "Api",
+     *                   description: "Fetch data from a Api",
+     *                   userId: "sNTKqvsS",
+     *                   requestConfig: {},
+     *                   persistData: false,
+     *                   fetchFrequency: 86400000,
+     *                   creationDate: "2019-12-06T16:12:21.944Z"
+     *                 }
+     *       401:
+     *         description: Unauthorized
+     *       500:
+     *         description: Internal Server Error
+     */
     this.router.route({
       method: 'post',
       path: '/',
@@ -58,6 +164,84 @@ export class DataSourceRouter {
       handler: this.controller.create.bind(this.controller),
     })
 
+    /**
+     * @swagger
+     *
+     * /data-source/{id}:
+     *   get:
+     *     security:
+     *       - cookieAuth: []
+     *     securitySchemes:
+     *       bearerAuth:
+     *         type: http
+     *         scheme: bearer
+     *         bearerFormat: JWT
+     *     parameters:
+     *       - in: path
+     *         name: id
+     *         required: true
+     *         schema:
+     *           type: string
+     *     produces:
+     *       - application/json
+     *     tags:
+     *       - Data Source
+     *     summary: Get data source
+     *     description: Get a specific data source by the given id.
+     *     responses:
+     *       200:
+     *         description: Created
+     *         content:
+     *           application/json:
+     *             schema:
+     *               required:
+     *               - data-source
+     *               properties:
+     *                 data-source:
+     *                   type: object
+     *                   required:
+     *                   - id
+     *                   - title
+     *                   - description
+     *                   - userId
+     *                   - requestConfig
+     *                   - creationDate
+     *                   - persistData
+     *                   - fetchFrequency
+     *                   properties:
+     *                     id:
+     *                       type: string
+     *                     title:
+     *                       type: string
+     *                     description:
+     *                       type: string
+     *                     userId:
+     *                       type: string
+     *                     requestConfig:
+     *                       type: json
+     *                     creationDate:
+     *                       type: string
+     *                     persistData:
+     *                       type: boolean
+     *                     fetchFrequency:
+     *                       type: number
+     *             examples:
+     *               filter:
+     *                 value: {
+     *                   id: "3cT4lb0M",
+     *                   title: "Api",
+     *                   description: "Fetch data from a Api",
+     *                   userId: "sNTKqvsS",
+     *                   requestConfig: {},
+     *                   persistData: false,
+     *                   fetchFrequency: 86400000,
+     *                   creationDate: "2019-12-06T16:12:21.944Z"
+     *                 }
+     *       401:
+     *         description: Unauthorized
+     *       500:
+     *         description: Internal Server Error
+     */
     this.router.route({
       method: 'get',
       path: '/:id',
@@ -87,6 +271,99 @@ export class DataSourceRouter {
       handler: this.controller.access.bind(this.controller),
     })
 
+    /**
+     * @swagger
+     *
+     * /data-source:
+     *   get:
+     *     security:
+     *       - cookieAuth: []
+     *     securitySchemes:
+     *       bearerAuth:
+     *         type: http
+     *         scheme: bearer
+     *         bearerFormat: JWT
+     *     produces:
+     *       - application/json
+     *     tags:
+     *       - Data Source
+     *     parameters:
+     *       - in: query
+     *         name: skip
+     *         schema:
+     *           type: number
+     *         description: Defines the offset of the requested data sources.
+     *       - in: query
+     *         name: take
+     *         schema:
+     *           type: number
+     *         description: Defines the amount of the requested data sources.
+     *     summary: List data source
+     *     description: List all the data sources.
+     *     responses:
+     *       200:
+     *         description: OK
+     *         content:
+     *           application/json:
+     *             schema:
+     *               required:
+     *               - data-sources
+     *               properties:
+     *                 total:
+     *                   type: number
+     *                 data-sources:
+     *                   type: object
+     *                   properties:
+     *                     data-source:
+     *                       type: object
+     *                       required:
+     *                       - id
+     *                       - title
+     *                       - description
+     *                       - userId
+     *                       - requestConfig
+     *                       - creationDate
+     *                       - persistData
+     *                       - fetchFrequency
+     *                       properties:
+     *                         id:
+     *                           type: string
+     *                         title:
+     *                           type: string
+     *                         description:
+     *                           type: string
+     *                         userId:
+     *                           type: string
+     *                         requestConfig:
+     *                           type: json
+     *                         creationDate:
+     *                           type: string
+     *                         persistData:
+     *                           type: boolean
+     *                         fetchFrequency:
+     *                           type: number
+     *             examples:
+     *               filter:
+     *                 value: {
+     *                   total: 1,
+     *                   data-sources: [
+     *                     {
+     *                       id: "3cT4lb0M",
+     *                       title: "Api",
+     *                       description: "Fetch data from a Api",
+     *                       userId: "sNTKqvsS",
+     *                       requestConfig: {},
+     *                       persistData: false,
+     *                       fetchFrequency: 86400000,
+     *                       creationDate: "2019-12-06T16:12:21.944Z"
+     *                     },
+     *                   ]
+     *                 }
+     *       401:
+     *         description: Unauthorized
+     *       500:
+     *         description: Internal Server Error
+     */
     this.router.route({
       method: 'get',
       path: '/',
@@ -119,6 +396,131 @@ export class DataSourceRouter {
       handler: this.controller.list.bind(this.controller),
     })
 
+    /**
+     * @swagger
+     *
+     * /data-source/{id}:
+     *   put:
+     *     security:
+     *       - cookieAuth: []
+     *     securitySchemes:
+     *       bearerAuth:
+     *         type: http
+     *         scheme: bearer
+     *         bearerFormat: JWT
+     *     produces:
+     *       - application/json
+     *     tags:
+     *       - Data Source
+     *     parameters:
+     *       - in: path
+     *         name: id
+     *         required: true
+     *         schema:
+     *           type: string
+     *     summary: Update data source
+     *     description: Update a data source by the given id.
+     *     requestBody:
+     *         content:
+     *           application/json:
+     *             schema:
+     *               required:
+     *               - data-source
+     *               properties:
+     *                 data-source:
+     *                   type: object
+     *                   required:
+     *                   - id
+     *                   - title
+     *                   - description
+     *                   - userId
+     *                   - requestConfig
+     *                   - creationDate
+     *                   - persistData
+     *                   - fetchFrequency
+     *                   properties:
+     *                     id:
+     *                       type: string
+     *                     title:
+     *                       type: string
+     *                     description:
+     *                       type: string
+     *                     userId:
+     *                       type: string
+     *                     requestConfig:
+     *                       type: json
+     *                     creationDate:
+     *                       type: string
+     *                     persistData:
+     *                       type: boolean
+     *                     fetchFrequency:
+     *                       type: number
+     *             examples:
+     *               filter:
+     *                 value: {
+     *                   id: "3cT4lb0M",
+     *                   title: "Api",
+     *                   description: "Fetch data from the api",
+     *                   userId: "sNTKqvsS",
+     *                   requestConfig: {},
+     *                   persistData: false,
+     *                   fetchFrequency: 86400000,
+     *                   creationDate: "2019-12-06T16:12:21.944Z"
+     *                 }
+     *     responses:
+     *       200:
+     *         description: Created
+     *         content:
+     *           application/json:
+     *             schema:
+     *               required:
+     *               - data-source
+     *               properties:
+     *                 data-source:
+     *                   type: object
+     *                   required:
+     *                   - id
+     *                   - title
+     *                   - description
+     *                   - userId
+     *                   - requestConfig
+     *                   - creationDate
+     *                   - persistData
+     *                   - fetchFrequency
+     *                   properties:
+     *                     id:
+     *                       type: string
+     *                     title:
+     *                       type: string
+     *                     description:
+     *                       type: string
+     *                     userId:
+     *                       type: string
+     *                     requestConfig:
+     *                       type: json
+     *                     creationDate:
+     *                       type: string
+     *                     persistData:
+     *                       type: boolean
+     *                     fetchFrequency:
+     *                       type: number
+     *             examples:
+     *               filter:
+     *                 value: {
+     *                   id: "3cT4lb0M",
+     *                   title: "Updated api",
+     *                   description: "Fetch data from the updated api",
+     *                   userId: "sNTKqvsS",
+     *                   requestConfig: {},
+     *                   persistData: true,
+     *                   fetchFrequency: 60000,
+     *                   creationDate: "2019-12-06T16:12:21.944Z"
+     *                 }
+     *       401:
+     *         description: Unauthorized
+     *       500:
+     *         description: Internal Server Error
+     */
     this.router.route({
       method: 'put',
       path: '/:id',
@@ -160,6 +562,80 @@ export class DataSourceRouter {
       handler: this.controller.update.bind(this.controller),
     })
 
+    /**
+     * @swagger
+     *
+     * /data-source/{id}:
+     *   delete:
+     *     security:
+     *       - cookieAuth: []
+     *     securitySchemes:
+     *       bearerAuth:
+     *         type: http
+     *         scheme: bearer
+     *         bearerFormat: JWT
+     *     produces:
+     *       - application/json
+     *     tags:
+     *       - Data Source
+     *     parameters:
+     *       - in: path
+     *         name: id
+     *         required: true
+     *         schema:
+     *           type: string
+     *     summary: Delete data source
+     *     description: Delete a data source by the given id.
+     *     responses:
+     *       200:
+     *         description: Created
+     *         content:
+     *           application/json:
+     *             schema:
+     *               required:
+     *               - data-source
+     *               properties:
+     *                 data-source:
+     *                   type: object
+     *                   required:
+     *                   - title
+     *                   - description
+     *                   - userId
+     *                   - requestConfig
+     *                   - creationDate
+     *                   - persistData
+     *                   - fetchFrequency
+     *                   properties:
+     *                     title:
+     *                       type: string
+     *                     description:
+     *                       type: string
+     *                     userId:
+     *                       type: string
+     *                     requestConfig:
+     *                       type: json
+     *                     creationDate:
+     *                       type: string
+     *                     persistData:
+     *                       type: boolean
+     *                     fetchFrequency:
+     *                       type: number
+     *             examples:
+     *               filter:
+     *                 value: {
+     *                   title: "Api",
+     *                   description: "Fetch data from the Api",
+     *                   userId: "sNTKqvsS",
+     *                   requestConfig: {},
+     *                   persistData: false,
+     *                   fetchFrequency: 86400000,
+     *                   creationDate: "2019-12-06T16:12:21.944Z"
+     *                 }
+     *       401:
+     *         description: Unauthorized
+     *       500:
+     *         description: Internal Server Error
+     */
     this.router.route({
       method: 'delete',
       path: '/:id',
