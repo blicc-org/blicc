1e01b340004cc184031943a4d535998a246d9150
Thilo Ilg <contact@thiloilg.com>
2019-12-25 19:07:59 +0100

    feat(app): add data source view

--- app/src/features/data-source-view/DataSourceView.jsx
+++ app/src/features/data-source-view/DataSourceView.jsx
@@ -1,5 +1,82 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import statusCode from 'http-status-codes'
+import { useApiEndpoint } from '../../common/hooks'
+import { MetaData } from '../../common/components/meta-data/MetaData'
 
 export function DataSourceView({ match }) {
-  return <h1>{match.params.id}</h1>
+  const path = `/data-sources/${match.params.id}`
+  const [, access] = useApiEndpoint(path)
+  const [data, setData] = useState({})
+
+  useEffect(() => {
+    async function fetchData() {
+      const [status, data] = await access()
+      if (status === statusCode.OK) {
+        setData(data)
+        console.log(data)
+      }
+    }
+    fetchData()
+    // eslint-disable-next-line
+  }, [match])
+
+  return (
+    <>
+      <MetaData
+        title={'data source view'}
+        description={'data source view ...'}
+        path={path}
+      />
+      <div className="container">
+        <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center my-3">
+          <h2 className="my-0">{data.title}</h2>
+        </div>
+        <div className="col px-0">
+          <div className="card">
+            <h5 className="card-header">Details</h5>
+            <div className="card-body">
+              <table style={{ width: '100%' }}>
+                <tbody>
+                  <tr>
+                    <td>
+                      <b>Title:</b>
+                    </td>
+                    <td>{data.title}</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <b>Description:</b>
+                    </td>
+                    <td>{data.description}</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <b>Persist data:</b>
+                    </td>
+                    <td>{data.persistData ? 'enabled' : 'disabled'}</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <b>Fetch frequency:</b>
+                    </td>
+                    <td>
+                      Every {data.fetchFrequency / (60 * 60 * 1000)} hours
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <b>Registration date:</b>
+                    </td>
+                    <td>
+                      {data.creationDate ? data.creationDate.split('T')[0] : ''}
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
