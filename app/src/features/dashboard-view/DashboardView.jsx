3dc66f6404c0229ca492fc12034a3b25ba0c78d6
Thilo Ilg <contact@thiloilg.com>
2020-01-03 14:30:55 +0100

    feat(app): remove ?edit query param

--- app/src/features/dashboard-view/DashboardView.jsx
+++ app/src/features/dashboard-view/DashboardView.jsx
@@ -56,6 +56,7 @@ export function DashboardView({ match, location }) {
         console.log('update was successful!')
       }
       setEdit(false)
+      setRedirect(`/dashboards/${match.params.id}`)
     } else {
       setEdit(true)
     }
