25bd14742a2edc78d0b717bab0a1272fbdebec34
Thilo Ilg <contact@thiloilg.com>
2020-01-13 18:59:05 +0100

    feat(app): add first cache then network for static ressources with limit

--- app/public/serviceWorker.js
+++ app/public/serviceWorker.js
@@ -1,11 +1,51 @@
-self.addEventListener('install', () => {
-  console.log('Service worker has been installed.')
-})
+const cacheName = 'v1'
+const limit = 50
 
-self.addEventListener('activate', () =>
-  console.log('Service worker has been activated.')
+self.addEventListener('install', () =>
+  console.log('Service worker has been installed')
 )
 
-self.addEventListener('fetch', () => {
-  console.log('fetch was falled')
+self.addEventListener('activate', () =>
+  console.log('Service worker has been activated')
+)
+
+self.addEventListener('fetch', evt => {
+  limitCacheSize('v1', 1)
+  const { request } = evt
+  const { url } = request
+  if (url.indexOf('http') === 0 && url.includes('chunk')) {
+    evt.respondWith(firstCacheThenNetwork(request))
+  }
 })
+
+async function firstCacheThenNetwork(request) {
+  const { url } = request
+  return await caches.match(request).then(res => {
+    if (res) console.log(`Service worker takes ${url} from cache`)
+    return (
+      res ||
+      fetch(request)
+        .then(res => {
+          const resClone = res.clone()
+          caches.open(cacheName).then(cache => {
+            cache.put(request, resClone)
+            limitCacheSize(cacheName, limit)
+          })
+          return res
+        })
+        .catch(err => {
+          console.log('Error in service worker: ', err)
+        })
+    )
+  })
+}
+
+function limitCacheSize(name, size) {
+  caches.open(name).then(cache => {
+    cache.keys().then(keys => {
+      if (keys.length > size) {
+        cache.delete(keys[0]).then(limitCacheSize(name, size))
+      }
+    })
+  })
+}
