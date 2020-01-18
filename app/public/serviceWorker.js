7d84522e2829f50ed39aeceedfe555a28a810d1a
Thilo Ilg <contact@thiloilg.com>
2020-01-18 23:47:10 +0100

    feat(app): add network first strategie

--- app/public/serviceWorker.js
+++ app/public/serviceWorker.js
@@ -1,26 +1,24 @@
 const cacheName = 'v1'
 const limit = 100
+const { scope } = registration
 
-self.addEventListener('install', () =>
-  console.log('Service worker has been installed')
-)
-
-self.addEventListener('activate', () =>
-  console.log('Service worker has been activated')
-)
+self.addEventListener('install', () => console.log('Service worker installed'))
+self.addEventListener('activate', () => console.log('Service worker activated'))
 
 self.addEventListener('fetch', evt => {
   const { request } = evt
   const { url } = request
   if (url.indexOf('http') === 0 && url.includes('chunk')) {
-    evt.respondWith(firstCacheThenNetwork(request))
+    evt.respondWith(cacheFirst(request))
+  } else if (url.includes(scope)) {
+    evt.respondWith(networkFirst(request))
   }
 })
 
-async function firstCacheThenNetwork(request) {
+async function cacheFirst(request) {
   const { url } = request
   return await caches.match(request).then(res => {
-    if (res) console.log(`Service worker takes ${url} from cache`)
+    if (res) console.log(`Service worker cache first ${url}`)
     return (
       res ||
       fetch(request)
@@ -33,10 +31,34 @@ async function firstCacheThenNetwork(request) {
           return res
         })
         .catch(err => {
-          console.log('Error in service worker: ', err)
+          console.log('Service worker cache first error: ', err)
         })
     )
   })
+}
+
+async function networkFirst(request) {
+  const { url } = request
+  return await fetch(request)
+    .then(res => {
+      const resClone = res.clone()
+      caches.open(cacheName).then(cache => {
+        cache.put(request, resClone)
+        limitCacheSize(cacheName, limit)
+      })
+      return res
+    })
+    .catch(async () => {
+      return await caches
+        .match(request)
+        .then(res => {
+          console.log(`Service worker network first ${url}`)
+          return res
+        })
+        .catch(err => {
+          console.log('Service worker network first error: ', err)
+        })
+    })
 }
 
 function limitCacheSize(name, size) {
