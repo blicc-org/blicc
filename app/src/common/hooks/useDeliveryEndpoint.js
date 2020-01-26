46d221b7db720949d9378d2b6a66602c67339405
Thilo Ilg <contact@thiloilg.com>
2020-01-26 15:12:20 +0100

    refactor(app): remove ref.current from data delivery hook

--- app/src/common/hooks/useDeliveryEndpoint.js
+++ app/src/common/hooks/useDeliveryEndpoint.js
@@ -1,9 +1,9 @@
-import { useEffect, useState, useRef, useContext, useCallback } from 'react'
+import { useEffect, useState, useContext, useCallback } from 'react'
 import uuid from 'uuid'
 import { SubscriberContext, AppContext } from '../context'
 import { DELIVERY } from '../../config'
 
-export let sockets = null
+export let socket = null
 
 export const WebSocketState = {
   [WebSocket.CONNECTING]: 'connecting',
@@ -16,37 +16,31 @@ export function useDeliveryEndpoint() {
   const [subscriberStack, setSubscriberStack] = useContext(SubscriberContext)
   const [appState] = useContext(AppContext)
   const { loggedIn } = appState
-  const ref = useRef(null)
-  const [state, setState] = useState(WebSocket.CLOSED)
+  const [state, setState] = useState(
+    socket && socket.readyState ? socket.readyState : WebSocket.CLOSED
+  )
 
   useEffect(() => {
-    if (loggedIn && ref.current === null) {
+    if (loggedIn && socket === null) {
       setState(WebSocket.CONNECTING)
-      sockets = new WebSocket(`${DELIVERY.ORIGIN}/connection`)
+      socket = new WebSocket(`${DELIVERY.ORIGIN}/connection`)
 
-      sockets.onopen = () => {
-        console.log('WebSocket connection opened')
+      socket.onopen = () => {
         setState(WebSocket.OPEN)
       }
 
-      sockets.onclose = () => {
-        console.log('WebSocket connection closed')
+      socket.onclose = () => {
+        socket = null
         setState(WebSocket.CLOSED)
       }
 
-      sockets.onerror = e => {
+      socket.onerror = e => {
         console.log(`An websocket error occured: ${JSON.stringify(e)}`)
-      }
-
-      ref.current = sockets
-
-      return () => {
-        sockets = {}
       }
     }
 
-    if (loggedIn && ref.current !== null) {
-      ref.current.onmessage = evt => {
+    if (loggedIn && socket !== null) {
+      socket.onmessage = evt => {
         const { channel, data } = JSON.parse(evt.data)
         if (channel && data && subscriberStack) {
           for (var key of Object.keys(subscriberStack)) {
@@ -56,21 +50,20 @@ export function useDeliveryEndpoint() {
       }
     }
 
-    if (!loggedIn && ref.current !== null) {
-      ref.current.close()
+    if (!loggedIn && socket !== null) {
+      socket.close()
       setState(WebSocket.CLOSING)
 
       return () => {
-        ref.current = null
+        socket = null
       }
     }
-  }, [loggedIn, subscriberStack])
+  }, [loggedIn, subscriberStack, state])
 
   const publish = useCallback(
     (channel, data) => {
-      console.log(channel, JSON.stringify(data))
       if (state === WebSocket.OPEN) {
-        ref.current.send(JSON.stringify({ channel, data }))
+        socket.send(JSON.stringify({ channel, data }))
       } else {
         console.log('Connection has to be open to publish!')
       }
