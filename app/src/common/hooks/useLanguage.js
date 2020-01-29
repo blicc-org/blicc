c915cbbf508b650b553fd23e7e4fe4001884a554
Thilo Ilg <contact@thiloilg.com>
2020-01-29 23:37:58 +0100

    fix(app): debug safari language settings

--- app/src/common/hooks/useLanguage.js
+++ app/src/common/hooks/useLanguage.js
@@ -4,7 +4,9 @@ import en from '../../languages/en'
 import de from '../../languages/de'
 
 export function useLanguage() {
-  const content = { de, en }
   const [appState] = useContext(AppContext)
-  return content[appState.language]
+  const content = { de, en }
+  let selected = en
+  if(appState && appState.language) selected = content[appState.language]
+  return selected
 }
