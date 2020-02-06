1eb4d865e1acdb82ef1370596dd94b01775a78c4
Thilo Ilg <contact@thiloilg.com>
2020-02-06 13:51:40 +0100

    feat(app): hide mobile nav on keyboard appearance

--- app/src/common/components/mobile-navigation/MobileNavigation.jsx
+++ app/src/common/components/mobile-navigation/MobileNavigation.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Layout, PieChart, Database } from 'react-feather'
 import { useMobile } from '../../hooks'
