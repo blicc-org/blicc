4b0ece9d2e05347222390b577b35429b622e1c93
Thilo Ilg <contact@thiloilg.com>
2020-02-06 11:43:27 +0100

    feat(app): testing mobile navigation bar

---
+++ app/src/common/components/mobile-navigation/MobileNavigation.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Layout, PieChart, Database } from 'react-feather'
+import './MobileNavigation.scss'
+
+export function MobileNavigation(){
+    return (
+        <div className="mobile-navigation">
+            <ul className="nav flex-row justify-content-around py-1">
+              <li className="nav-item">
+                <Link className="nav-link" to="/dashboards">
+                  <Layout className="feather" />
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/charts">
+                  <PieChart className="feather" /> 
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/data-sources">
+                  <Database className="feather" />
+                </Link>
+              </li>
+            </ul>
+        </div>
+    )
+}
