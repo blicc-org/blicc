7202fb8892060611fe9309e833982fb7c91d8441
Thilo Ilg <contact@thiloilg.com>
2020-02-16 15:19:17 +0100

    feat(app): test update app behavior

--- app/src/features/landing-page/LandingPage.jsx
+++ app/src/features/landing-page/LandingPage.jsx
@@ -7,7 +7,7 @@ import { ReactComponent as Logo } from '../../assets/img/Logo.svg'
 export function LandingPage() {
   const content = useLanguage()
   const title = 'Blicc'
-  const description = 'Visualize your data with customizable dashboards. xD'
+  const description = 'Visualize your data with customizable dashboards.'
   const path = '/'
 
   // close websocket connection on logged out
@@ -19,7 +19,7 @@ export function LandingPage() {
       <div className="col-md-5 mx-auto py-5 my-5 text-center">
         <Logo height={'100px'} alt="logo" />
         <h1 className="display-4 font-weight-normal">{content.title}</h1>
-        <p className="lead font-weight-normal">{content.description}</p>
+        <p className="lead font-weight-normal">{content.description} xD</p>
         <Link className="btn btn-outline-primary" to="/dashboards">
           {content.getStarted}
         </Link>
