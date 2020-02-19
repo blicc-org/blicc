ad87dbb7e017341768505883ac555a108e833330
Thilo Ilg <contact@thiloilg.com>
2020-02-19 11:49:40 +0100

    feat(app): center loading spinner

--- app/src/common/components/loading/Loading.jsx
+++ app/src/common/components/loading/Loading.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
 export function Loading() {
+  const style = {
+    alignSelf: 'center',
+    margin: 'auto',
+  }
   return (
-    <div className="spinner-border text-primary" role="status">
+    <div className="spinner-border text-primary" style={style} role="status">
       <span className="sr-only">Loading...</span>
     </div>
   )
