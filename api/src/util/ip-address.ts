cb937681df5582167de81be3f4facbd0077c0187
Thilo Ilg <contact@thiloilg.com>
2020-02-21 18:59:36 +0100

    feat(api): change var to const or let

--- api/src/util/ip-address.ts
+++ api/src/util/ip-address.ts
@@ -2,12 +2,12 @@ import os from 'os'
 
 export class IpAddress {
   public static access(): string {
-    var interfaces = os.networkInterfaces()
-    for (var devName in interfaces) {
-      var iface = interfaces[devName]
+    const interfaces = os.networkInterfaces()
+    for (const devName in interfaces) {
+      const iface = interfaces[devName]
 
-      for (var i = 0; i < iface.length; i++) {
-        var alias = iface[i]
+      for (let i = 0; i < iface.length; i++) {
+        const alias = iface[i]
         if (
           alias.family === 'IPv4' &&
           alias.address !== '127.0.0.1' &&
