778ba36c390f1a6fba719196bab6096c89592b74
Thilo Ilg <contact@thiloilg.com>
2020-02-21 23:34:11 +0100

    feat(api): add healthcheck

--- mock-api/src/api.ts
+++ mock-api/src/api.ts
@@ -1,6 +1,7 @@
 import Koa from 'koa'
 import logger from 'koa-logger'
-import { ExternalApiMock } from './external-api-mock/external-api-mock.router'
+import { ApiMockRouter } from './external-api-mock/api-mock.router'
+import { HealthCheckRouter } from './health-check/health-check.router'
 
 export class Api extends Koa {
   public constructor() {
@@ -8,6 +9,7 @@ export class Api extends Koa {
 
     this.proxy = true // forward for TSL encryption on proxy level
     this.use(logger())
-    this.use(new ExternalApiMock('/').routes())
+    this.use(new ApiMockRouter('/').routes())
+    this.use(new HealthCheckRouter('/health-check').routes())
   }
 }
--- mock-api/src/health-check/health-check.router.ts
+++ mock-api/src/health-check/health-check.router.ts
@@ -1,20 +1,20 @@
 import Router, { IMiddleware as Middleware } from 'koa-router'
-import { DataDeliveryMockController } from './external-api-mock'
+import { HealthCheckController } from './health-check.controller'
 
-export class ExternalApiMock {
+export class HealthCheckRouter {
   private prefix: string
   private router: Router
-  private controller: DataDeliveryMockController
+  private controller: HealthCheckController
 
   public constructor(prefix: string) {
     this.prefix = prefix
     this.router = new Router()
-    this.controller = new DataDeliveryMockController()
+    this.controller = new HealthCheckController()
   }
 
   public routes(): Middleware {
     this.router.prefix(this.prefix)
-    this.router.get('/', this.controller.mock.bind(this.controller))
+    this.router.get('/', this.controller.check.bind(this.controller))
 
     return this.router.routes()
   }
