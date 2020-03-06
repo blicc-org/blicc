e28c2e11144a1615d9cd4e6d41392a284bb5ecd3
Thilo Ilg <contact@thiloilg.com>
2020-03-06 21:36:02 +0100

    feat(plugins): add default env file

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 # initialize setup
 
+## check if git, docker, docker-compose and yarn is installed
+git --version >/dev/null || echo 'dependency missing: please install git'
+
 ## generate certificates into certs folder
 mkdir -p certs
 openssl genrsa -out ./certs/rsa.pem 2048
@@ -8,3 +11,6 @@ openssl rsa -in rsa.pem -outform PEM -pubout -out ./certs/rsa_pub.pem
 
 ## create default .env file
 cp ./scripts/.env.default .env
+
+## init subprojects
+cd plugins && sh ./scripts/init.sh
