c9c90dc8a5446585f3f6a0bac0c020ce12ccbb15
Thilo Ilg <contact@thiloilg.com>
2020-03-08 12:19:06 +0100

    feat(all): remove unnecessary scripts

--- scripts/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# build and run
-
-## build and run docker compose
-docker-compose up -d --build
-
-## deploy plugins into redis
-for ((;;)); do curl -s http://localhost > /dev/null && break; sleep 1; done && yarn --cwd 'plugins' deploy
