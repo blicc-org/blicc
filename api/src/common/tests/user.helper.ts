54c88ac81499eb220c689a65b98dfab8c74270f2
Thilo Ilg <contact@thiloilg.com>
2020-03-16 15:26:50 +0100

    feat(api): update dependencies

--- api/src/common/tests/user.helper.ts
+++ api/src/common/tests/user.helper.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import {v4 as uuid} from 'uuid'
+import { v4 as uuid } from 'uuid'
 import { user } from '../../features/user/mocks/user.mock'
 import { API_TEST_TARGET, ADMIN_MAIL, ADMIN_PASSWORD } from '../../config'
 
