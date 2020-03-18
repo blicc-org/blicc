6500616c91b77a2ae1c4c747229d34b30652f7dd
Thilo Ilg <contact@thiloilg.com>
2020-03-18 22:27:32 +0100

    fix(mock-api): fix routes

--- mock-api/src/api.ts
+++ mock-api/src/api.ts
@@ -1,8 +1,9 @@
 import Koa from 'koa'
 import logger from 'koa-logger'
 import { TimeRouter } from './time/time.router'
-import { HealthCheckRouter } from './health-check/health-check.rounter'
 import { WeatherRouter } from './weather/weather.router'
+import { ManufacturingDataRouter } from './manufacturing-data/manufacturing-data.router'
+import { HealthCheckRouter } from './health-check/health-check.rounter'
 
 export class Api extends Koa {
   public constructor() {
@@ -12,7 +13,7 @@ export class Api extends Koa {
     this.use(logger())
     this.use(new TimeRouter('/time').routes())
     this.use(new WeatherRouter('/weather').routes())
-    this.use(new TimeRouter('/manufacturing-data').routes())
+    this.use(new ManufacturingDataRouter('/manufacturing-data').routes())
     this.use(new HealthCheckRouter('/health-check').routes())
   }
 }
