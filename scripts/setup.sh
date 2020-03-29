fe03b6c5a4634548e7b87b486ed790197cbbef28
Thilo Ilg <contact@thiloilg.com>
2020-03-29 14:32:39 +0200

    feat(rabbitmq): mount container

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,7 +8,7 @@ fdisk -l
 mount /dev/sdb /srv
 
 ## create directories to work with docker mount
-mkdir redis-data postgres-data mongo-data prometheus-data
+mkdir redis-data postgres-data mongo-data prometheus-data rabbitmq-data
 
 # login to docker registry
 docker login registry.example.com
