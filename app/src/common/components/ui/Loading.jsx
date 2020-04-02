e94063ff6974f7ffdc12ce44e324637844f9b38c
Thilo Ilg <contact@thiloilg.com>
2020-04-02 18:06:23 +0200

    feat(app): change loading spinner color

--- app/src/common/components/ui/Loading.jsx
+++ app/src/common/components/ui/Loading.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import { lightgray } from '../../../Theme.scss'
 
 export function Loading() {
   const style = {
     alignSelf: 'center',
     margin: 'auto',
+    color: lightgray,
   }
   return (
-    <div className="spinner-border text-primary" style={style} role="status">
+    <div className="spinner-border" style={style} role="status">
       <span className="sr-only">Loading...</span>
     </div>
   )
