db611f843b9f9fe9ef2840915e8b6726955f94bf
Thilo Ilg <contact@thiloilg.com>
2020-04-08 17:32:19 +0200

    fix(api): remove unused import

--- api/src/features/profile-picture/profile-picture.router.ts
+++ api/src/features/profile-picture/profile-picture.router.ts
@@ -1,5 +1,5 @@
 import { Middleware } from 'koa'
-import createRouter, { Router, Joi } from 'koa-joi-router'
+import createRouter, { Router } from 'koa-joi-router'
 import { ProfilePictureController } from './profile-picture.controller'
 import {
   AuthMiddleware,
