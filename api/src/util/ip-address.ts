97f4a6b5048a173c6e18577723b915b437c611e9
Thilo Ilg <contact@thiloilg.com>
2020-04-20 16:19:00 +0200

    feat(api): update dependencies

--- api/src/util/ip-address.ts
+++ api/src/util/ip-address.ts
@@ -5,15 +5,16 @@ export class IpAddress {
     const interfaces = os.networkInterfaces()
     for (const devName in interfaces) {
       const iface = interfaces[devName]
-
-      for (let i = 0; i < iface.length; i++) {
-        const alias = iface[i]
-        if (
-          alias.family === 'IPv4' &&
-          alias.address !== '127.0.0.1' &&
-          !alias.internal
-        )
-          return alias.address
+      if (iface) {
+        for (let i = 0; i < iface.length; i++) {
+          const alias = iface[i]
+          if (
+            alias.family === 'IPv4' &&
+            alias.address !== '127.0.0.1' &&
+            !alias.internal
+          )
+            return alias.address
+        }
       }
     }
 
