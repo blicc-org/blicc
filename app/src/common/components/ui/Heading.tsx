2994e36dcb5bcf37ad46c9a8ff330ddea5a2bebe
Thilo Ilg <contact@thiloilg.com>
2020-04-22 11:54:47 +0200

    feat(app): always same heading height

--- app/src/common/components/ui/Heading.tsx
+++ app/src/common/components/ui/Heading.tsx
@@ -1,4 +1,5 @@
 import React, { ReactElement } from 'react'
+import styles from './Heading.module.scss'
 
 interface Props {
   title: string
@@ -8,7 +9,7 @@ interface Props {
 export function Heading({ title, children }: Props): ReactElement {
   return (
     <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center my-3">
-      <h2 className="my-2">{title}</h2>
+      <h2 className={`my-2 ${styles.title}`}>{title}</h2>
       <div className="btn-toolbar">{children}</div>
     </div>
   )
