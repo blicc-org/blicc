e09a866c3ab77ff3480d5162cc41797c930db02f
Thilo Ilg <contact@thiloilg.com>
2020-04-24 18:07:29 +0200

    feat(scripts): update scripts for ubuntu 20.04

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,20 +1,8 @@
 #!/bin/sh
 # initialize ubuntu server
 
-## scp -r ./scripts/server.sh root@$IP:/root/init.sh
-## ssh -t root@$IP "sh init.sh"
-
-sudo apt -y update
-sudo apt -y install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-sudo apt -y update
-apt-cache policy docker-ce
-sudo apt -y install docker-ce
-
-sudo curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-docker-compose --version
+apt -y update
+apt -y install docker.io=19.03.8-0ubuntu1
 
 ## file for traefik to store certificates
 touch acme.json
