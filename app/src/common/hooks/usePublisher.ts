83e7138a7dc3e78c0bc4b6ca6c849230cd7ca5aa
Thilo Ilg <contact@thiloilg.com>
2020-05-03 10:24:45 +0200

    feat(app): remove extra calls against data sources

--- app/src/common/hooks/usePublisher.ts
+++ app/src/common/hooks/usePublisher.ts
@@ -1,5 +1,3 @@
-import { useApiEndpoint } from './useApiEndpoint'
-import { API } from '../../config'
 import { useDeliveryEndpoint } from './useDeliveryEndpoint'
 import { useContext } from 'react'
 import { SettingsContext } from '../context'
@@ -7,8 +5,6 @@ import { SettingsContext } from '../context'
 export function usePublisher(): Array<Function> {
   const [settings] = useContext(SettingsContext)
   const [publish] = useDeliveryEndpoint()
-  const [, access] = useApiEndpoint()
-  const interval = 5000 // live time update interval
 
   function retrieveIds(): any {
     const set = []
@@ -24,24 +20,14 @@ export function usePublisher(): Array<Function> {
     if (ids.length > 0) {
       ids.map(
         async (id: string): Promise<void> => {
-          const [status, data] = await access({
-            url: `${API.ORIGIN}/data-sources/${id}`,
-          })
-          if (status === 200) {
-            await publish(`/data-sources/${id}`)
-          }
+          await publish(`/data-sources/${id}`)
         }
       )
     }
   }
 
   async function publishById(id: string): Promise<void> {
-    const [status, data] = await access({
-      url: `${API.ORIGIN}/data-sources/${id}`,
-    })
-    if (status === 200) {
-      await publish(`/data-sources/${id}`)
-    }
+    await publish(`/data-sources/${id}`)
   }
 
   return [publishAll, publishById]
