02232b739525851da553f09ce10a95594bfdfad1
Thilo Ilg <contact@thiloilg.com>
2020-05-15 14:45:26 +0200

    feat(app): refactor subscribe function

--- app/src/common/hooks/useEndpointWebSocket.ts
+++ app/src/common/hooks/useEndpointWebSocket.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState, useCallback, useRef } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { v4 as uuid } from 'uuid'
 import { DELIVERY } from '../../config'
 
