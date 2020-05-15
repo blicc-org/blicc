7f94f828097e8efd16d0f7a2029bfb21d3632822
Thilo Ilg <contact@thiloilg.com>
2020-05-15 17:47:12 +0200

    fix(app): fix websocket endpoint hook

--- app/src/common/hooks/useEndpointWebSocket.ts
+++ app/src/common/hooks/useEndpointWebSocket.ts
@@ -11,64 +11,64 @@ interface Stack<T> {
   [key: string]: T
 }
 
+export let incoming: Stack<Data> = {}
+export let outgoing: Stack<Data> = {}
+
 export function useEndpointWebSocket(): [Publish, Subscribe] {
-  const [callbackStack, setCallbackStack] = useState<Stack<Callback>>({})
-  const [outgoing, setOutgoing] = useState<Stack<Data>>({})
-  const [incoming, setIncoming] = useState<Stack<Data>>({})
+  const url = `${DELIVERY.ORIGIN_WEBSOCKET}/connection`
   const wb = useRef<WebSocket | null>()
+  const [stack, setStack] = useState<Stack<Callback>>({})
 
   useEffect(() => {
-    if (wb.current) {
-      wb.current.onmessage = (evt: any): void => {
-        const { channel, data } = JSON.parse(evt.data)
-        setIncoming((prev) => ({ ...prev, [channel]: data }))
-        if (channel && data) {
-          Object.keys(callbackStack).forEach((key) => {
-            if (key.includes(channel)) callbackStack[key](data)
-          })
-        }
-      }
-    } else {
-      wb.current = new WebSocket(`${DELIVERY.ORIGIN_WEBSOCKET}/connection`)
+    if (!wb.current) wb.current = new WebSocket(url)
 
-      wb.current.onopen = (): void => {
-        Object.keys(outgoing).forEach((key) => {
-          const payload = JSON.stringify({
-            channel: key,
-            data: outgoing[key],
-          })
-          if (wb.current) wb.current.send(payload)
+    wb.current.onopen = (): void => {
+      Object.keys(outgoing).forEach((key) => {
+        const payload = JSON.stringify({
+          channel: key,
+          data: outgoing[key],
+        })
+        if (wb.current) wb.current.send(payload)
+      })
+    }
+
+    wb.current.onmessage = (evt: any): void => {
+      const { channel, data } = JSON.parse(evt.data)
+      incoming[channel] = data
+      if (channel && data) {
+        Object.keys(stack).forEach((key) => {
+          if (key.includes(channel)) stack[key](data)
         })
       }
-
-      wb.current.onclose = (): void => {
-        wb.current = null
-      }
-
-      wb.current.onerror = (evt: Event): void => console.log(evt)
     }
-  }, [incoming, outgoing, callbackStack])
+
+    wb.current.onclose = (): void => {
+      wb.current = null
+    }
+
+    wb.current.onerror = (evt: Event): void => console.log(evt)
+  }, [stack])
 
   useEffect(() => {
     return (): void => {
-      if (wb.current) {
-        wb.current.close()
-        wb.current = null
-      }
+      if (wb.current) wb.current.close()
+      wb.current = null
+      incoming = outgoing = {}
     }
   }, [])
 
   const publish: Publish = (channel: string, data: any = null): void => {
     if (wb.current && wb.current.readyState === WebSocket.OPEN) {
-      wb.current.send(JSON.stringify(data ? { channel, data } : { channel }))
+      const msg = JSON.stringify(data ? { channel, data } : { channel })
+      wb.current.send(msg)
     } else {
-      setOutgoing((prev) => ({ ...prev, [channel]: data }))
+      outgoing[channel] = data
     }
   }
 
   const subscribe: Subscribe = (channel, callback) => {
     const key = channel + uuid()
-    setCallbackStack((prev) => ({ ...prev, [key]: callback }))
+    setStack((prev) => ({ ...prev, [key]: callback }))
     return incoming[channel]
   }
 
